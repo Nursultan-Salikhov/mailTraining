c8d2b18ddbdec485b8d1d2f80dfc9cc8f0a9fc62
Nursultan-Salikhov <84440114+Nursultan-Salikhov@users.noreply.github.com>
2022-12-02 15:06:06 +0300

    Added example code.

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"crypto/tls"
+	"fmt"
+	"gopkg.in/gomail.v2"
+)
 
+func main() {
+	m := gomail.NewMessage()
+
+	m.SetHeader("From", "from_example@yandex.ru")
+	m.SetHeader("To", "to_example@mail.ru")
+	m.SetHeader("Subject", "go test")
+
+	m.SetBody("text/plain", "This is example text \n Just text")
+
+	// mail host "smtp.mail.ru", port 465
+	// yandex host "smtp.yandex.ru", port 465
+	// google host "smtp.gmail.com", port 587
+	d := gomail.NewDialer("smtp.yandex.ru", 465, "from_example@yandex.ru", "external_password")
+
+	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+	if err := d.DialAndSend(m); err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	fmt.Println("Sending message successful")
 }
